fec66de06994bf095a0d82916321d07609b817bd
Dhruv Ghadiali <86670107+21DG@users.noreply.github.com>
2022-04-12 03:26:30 +0530

    UI design for 12-04-2022

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css';
+import logo from './assets/logo.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 1
-        </a>
-      </header>
-    </div>
+    <section class="h-100 gradient-form" style={{ backgroundColor: "#eee" }}>
+      <div class="container py-5 h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-xl-10">
+            <div class="card rounded-3 text-black">
+              <div class="row g-0">
+                <div class="col-lg-6">
+                  <div class="card-body p-md-5 mx-md-4">
+
+                    <div class="text-center">
+                      <img src={logo}
+                        className="rounded-circle mb-3"
+                        style={{ width: "185px" }} alt="logo" />
+                      <h3 class="mt-1 mb-5 pb-1">We are Labdhi Vinay Team</h3>
+                    </div>
+
+                    <form>
+                      <h5>Please login to your account</h5>
+                      <div class="form-outline mb-4 mt-4">
+                        <input type="email" id="form2Example11" class="form-control" placeholder="Email address" />
+                      </div>
+
+                      <div class="form-outline mb-4">
+                        <input type="password" id="form2Example22" class="form-control" placeholder="*** **** **"/>
+                      </div>
+
+                      <div class="pt-1 mb-5 pb-1">
+                        <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">
+                          LOGIN
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                  <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                    <h4 class="mb-4">We are more than just a company</h4>
+                    <p class="small mb-0">
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
